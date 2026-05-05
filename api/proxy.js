60552b816c81e8f2e49cac0f4f54577896e8c890
labmodel-geoteg-uerj-geo <labmodel.uerj.geo@gmail.com>
2026-05-04 22:48:42 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,17 +1,16 @@
 export default async function handler(req, res) {
-  const query = req.url.split('?')[1] || '';
-  const url = `http://207.246.112.88:8080/geoserver/wms?${query}`;
-
   try {
+    const url = "http://207.246.112.88:8080/geoserver/ide/wms?" + new URL(req.url, "http://localhost").searchParams.toString();
+
     const response = await fetch(url);
 
-    const contentType = response.headers.get("content-type") || "image/png";
+    const contentType = response.headers.get("content-type");
     const buffer = await response.arrayBuffer();
 
     res.setHeader("Content-Type", contentType);
     res.send(Buffer.from(buffer));
 
-  } catch (err) {
-    res.status(500).send("Erro no proxy: " + err.message);
+  } catch (error) {
+    res.status(500).send("Erro no proxy: " + error.message);
   }
 }
