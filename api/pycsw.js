1629de11f8237f6ea8e18c07a8dfe1f19d9f56e0
labmodel-geoteg-uerj-geo <labmodel.uerj.geo@gmail.com>
2026-05-06 17:12:14 -0300

    Update pycsw.js

--- api/pycsw.js
+++ api/pycsw.js
@@ -1,10 +1,20 @@
-export default function handler(req, res) {
-  const url = "http://207.246.112.88:8000/csw?" + req.url.split('?')[1];
+export default async function handler(req, res) {
+  try {
+    // pega parâmetros corretamente
+    const params = new URLSearchParams(req.query).toString();
 
-  fetch(url)
-    .then(r => r.text())
-    .then(data => {
-      res.setHeader("Content-Type", "text/xml");
-      res.send(data);
-    });
+    const url = `http://207.246.112.88:8000/csw?${params}`;
+
+    console.log("Chamando:", url);
+
+    const response = await fetch(url);
+    const data = await response.text();
+
+    res.setHeader("Content-Type", "text/xml");
+    res.status(200).send(data);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro no proxy pycsw");
+  }
 }
