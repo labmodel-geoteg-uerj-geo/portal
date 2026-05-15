ff03c5f067190c4aa3d63669140f2374adb9e9b9
labmodel-geoteg-uerj-geo <labmodel.uerj.geo@gmail.com>
2026-05-14 21:23:56 -0300

    Update catalogo.js

--- api/catalogo.js
+++ api/catalogo.js
@@ -1,9 +1,20 @@
-export default async function handler(req, res) {
-  const response = await fetch("http://207.246.112.88:9000/catalogo");
+module.exports = async function handler(req, res) {
+  try {
+    const response = await fetch("http://207.246.112.88:9000/catalogo");
 
-  const data = await response.text();
+    const data = await response.text();
 
-  res.status(response.status);
-  res.setHeader("Content-Type", "application/json");
-  res.send(data);
-}
+    res.statusCode = response.status;
+    res.setHeader("Content-Type", "application/json");
+    res.end(data);
+
+  } catch (error) {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+
+    res.end(JSON.stringify({
+      error: "Erro ao buscar catálogo na FastAPI",
+      details: error.message
+    }));
+  }
+};
